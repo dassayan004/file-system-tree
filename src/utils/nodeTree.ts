bbeb43f7218adcb449052ca9b1ae788c30088e97
Sayan <91191550+dassayan004@users.noreply.github.com>
2025-08-20 15:58:07 +0530

    gg

--- src/utils/nodeTree.ts
+++ src/utils/nodeTree.ts
@@ -47,7 +47,136 @@ export const nodes: NodeTree[] = [
       },
       {
         name: "Projects",
-        nodes: [],
+        nodes: [
+          {
+            name: "gql-try",
+            nodes: [
+              { name: "bun.lock" },
+              { name: "codegen.ts" },
+              { name: "components.json" },
+              { name: "graphql.schema.json" },
+              { name: "next.config.ts" },
+              { name: "next-env.d.ts" },
+              { name: "package.json" },
+              { name: "postcss.config.mjs" },
+              {
+                name: "public",
+                nodes: [
+                  { name: "file.svg" },
+                  { name: "globe.svg" },
+                  { name: "next.svg" },
+                  { name: "vercel.svg" },
+                  { name: "window.svg" },
+                ],
+              },
+              { name: "README.md" },
+              {
+                name: "src",
+                nodes: [
+                  {
+                    name: "app",
+                    nodes: [
+                      {
+                        name: "(app)",
+                        nodes: [{ name: "layout.tsx" }, { name: "page.tsx" }],
+                      },
+                      {
+                        name: "(auth)",
+                        nodes: [
+                          {
+                            name: "auth",
+                            nodes: [{ name: "page.tsx" }],
+                          },
+                          { name: "layout.tsx" },
+                        ],
+                      },
+                      { name: "favicon.ico" },
+                      { name: "globals.css" },
+                      { name: "layout.tsx" },
+                    ],
+                  },
+                  {
+                    name: "components",
+                    nodes: [
+                      { name: "core", nodes: [] },
+                      {
+                        name: "ui",
+                        nodes: [
+                          { name: "button.tsx" },
+                          { name: "card.tsx" },
+                          {
+                            name: "code-block",
+                            nodes: [
+                              { name: "index.tsx" },
+                              { name: "server.tsx" },
+                            ],
+                          },
+                          { name: "code-editor.tsx" },
+                          { name: "form.tsx" },
+                          { name: "input.tsx" },
+                          { name: "label.tsx" },
+                          { name: "passwordfield.tsx" },
+                          { name: "select.tsx" },
+                          { name: "sonner.tsx" },
+                          { name: "spinner.tsx" },
+                          { name: "tabs.tsx" },
+                          { name: "theme-animations.ts" },
+                          { name: "theme-toggle-button.tsx" },
+                        ],
+                      },
+                    ],
+                  },
+                  {
+                    name: "db",
+                    nodes: [
+                      {
+                        name: "auth",
+                        nodes: [{ name: "authSlice.ts" }],
+                      },
+                      { name: "hooks.redux.ts" },
+                      { name: "storage.ts" },
+                      { name: "store.ts" },
+                    ],
+                  },
+                  {
+                    name: "gql",
+                    nodes: [
+                      { name: "fragment-masking.ts" },
+                      { name: "gql.ts" },
+                      { name: "graphql.ts" },
+                      { name: "index.ts" },
+                    ],
+                  },
+                  {
+                    name: "lib",
+                    nodes: [
+                      { name: "ApolloWrapper.tsx" },
+                      { name: "providers.tsx" },
+                      { name: "route-guard.ts" },
+                      { name: "theme-provider.tsx" },
+                      { name: "type.ts" },
+                      { name: "utils.ts" },
+                    ],
+                  },
+                  {
+                    name: "service",
+                    nodes: [
+                      {
+                        name: "action",
+                        nodes: [{ name: "userAction.ts" }],
+                      },
+                      {
+                        name: "queries",
+                        nodes: [{ name: "user.graphql" }],
+                      },
+                    ],
+                  },
+                ],
+              },
+              { name: "tsconfig.json" },
+            ],
+          },
+        ],
       },
       { name: "passwords.tsx" },
       { name: "input.sh" },
